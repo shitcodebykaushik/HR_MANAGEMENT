b4aa3bec471af411d878f2f9d742f0cabe793788
Kaushik raj <algorithmunloack@gmail.com>
2024-07-03 23:45:33 +0530

    The syntax error is bothering

--- main.go
+++ main.go
@@ -75,8 +75,7 @@ func main () {
 		} 
 		 var employess []Employee =make ([]Employee, 0)
 
-        if err := cursor.All(c.Context(),&employess);
-		err! = nil {
+        if err := cursor.All(c.Context(),&employess); err != nil {
          return c.Status(500).SendString(err.Error())
 		
 		}
@@ -120,9 +119,64 @@ func main () {
 
 
 
-	app.Put("/employee/:id")
-	app.Delete("/employee/:id")
+	app.Put("/employee/:id".func(c *fiber.Ctx) error {
+		idParam := c.Params("id")
+		employeeID,err := primitive.ObjectIDFromHex (idParam)
+        
+		if err !=nil {
+			return c.SendStatus(400)
+		}
+		employee := new(Employee) 
+
+		if err := c.BodyParser(employee); err !=nil {
+			retrun c.Status(400).SendString(err.Error())
+		}
+
+		query := bson.D {{Key:"_id",Value: emloyeeID}}
+		update :=bson.D{
+			{
+				Key: "$set",
+				 Value: bson.D{
+					{Key:"name",Value:employee.Name},
+					{Key: "age",Value:employee.Age},
+					{Key: "salary",Value:employee.Salary},
+				 },
+			},
+		}
+		err = mg.Db.Collection("employees").FindOneAndUpdate(c.context(),query, update).Err() 
+		if err !=nil {
+			if err ==momgo.ErrNoDocuments {
+				return c.SendStatus((400))
+			}
+			return c.SendStatus(500)
+		}
+
+		employee.ID = idParam
 
 
+		return c.Status(200).JSON(employee)
+	})
+ 
+	app.Delete("/employee/:id",func (c *fiber.Ctx)error {
+		empemployeeID,err :=  primitive.ObjectIDFromHex(c.Params("id"),)
 
+		if err!=nil {
+			return c.SendStatus(400)
+		}
+			query := bson.D{{Key:"_id",Value: employeeID}}
+		 result,err :=	mg.Db.Collection("employees").DeleteOne(c.Context(),&query)
+	     
+
+		 if err !=nil {
+			return c.SendStatus(500)
+		 }
+		 if result.DeletedCount<1{
+			return c.SendStatus(404)
+		 }
+         return c.Status((200)).JSON("record deleted")
+		 }) 
+	log.Fatal (app.Listen(":3000"))
 }
+
+
+
